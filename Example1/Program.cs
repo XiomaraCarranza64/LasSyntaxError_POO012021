cd766e4bfd1737756986a797ced637fba0d84634
XiomaraCarranza64 <00035420@uca.edu.sv>
2021-03-11 00:40:34 -0600

    EXAMPLE1

---
+++ Example1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0; 
+            int b = 0; 
+            Console.Write("Type a whole number: ");
+            a= Int32.Parse(Console.ReadLine()); 
+            Console.Write("Type another whole number: ");
+            b= Int32.Parse(Console.ReadLine()); 
+            Console.WriteLine("LOADING... ");
+            Console.WriteLine($"Adding {a} + {b} equals {AddNumber(a,b)} ");
+
+
+
+        }
+
+        static int AddNumber(int a, int b)
+        {
+            return a+b; 
+        }
+    }
+}
